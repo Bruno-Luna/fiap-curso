cc3f192aa30fed71e0bdede88bd09c7bb84ec837
Bruno Luna <luna.brsilva@gmail.com>
2021-12-01 09:23:44 -0300

    Conversões

---
+++ Curso-fiap/src/br/com/fiap/curso/Conversoes.java
@@ -0,0 +1,19 @@
+package br.com.fiap.curso;
+
+public class Conversoes {
+
+	public static void main(String[] args) {
+
+		double valor = 9.99;
+		int  valorInt = (int) valor;
+		System.out.println(valorInt);
+		
+		//Essa conversão é viável - 
+		//porém há risco em perder informações. 
+		//Esse tipo de conversão é possível por  meio  de cast.  
+		//A  sintaxe  é  adicionar  o  tipo  que  queremos  converter 
+		//entre parênteses
+		
+	}
+
+}
