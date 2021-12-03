6605e815f5f14ee01f2ad85bbdb366e2b7a0d8ee
Bruno Luna <luna.brsilva@gmail.com>
2021-12-03 10:52:39 -0300

    Documentação

--- SistemaBancario/src/br/com/fiap/sistemabancario/Conta.java
+++ SistemaBancario/src/br/com/fiap/sistemabancario/Conta.java
@@ -1,5 +1,9 @@
 package br.com.fiap.sistemabancario;
-
+/**
+ * Classe que abstrai uma Conta Bancária
+ * @author Bruno Luna
+ * @version 1.0
+ */
 public class Conta {
 
 	int agencia;
@@ -14,14 +18,27 @@ public class Conta {
 		this.saldo = saldo;
 	}
 	
+	/**
+	 * Acrescenta um determinado valor ao saldo da conta
+	 * @param valor a ser colocado
+	 */
 	public void depositar(double valor) {
 		this.saldo += valor;
 	}
 	
+	/**
+	 * Retirada de um determinado valor do saldo da conta
+	 * @param valor que sera subtraido do saldo
+	 * @see depositar
+	 */
 	public void sacar(double valor) {
 		this.saldo -= valor;
 	}
 	
+	/**
+	 * Verifica o saldo da conta
+	 * @return Valor do saldo da conta
+	 */
 	public double verificarExtrato() {
 		return this.saldo;
 	}
