987a41981ac717b2bc720863b78e0255d4fc9be0
Bruno Luna <luna.brsilva@gmail.com>
2021-12-10 16:40:00 -0300

    JDBC - Conexão com o db

---
+++ jdbc-test/src/JdbcTest.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 
+ * @author Bruno Luna
+ *
+ */
+public class JdbcTest {
+
+	public static void main(String[] args)  throws SQLException{
+		
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myResul = null;
+		
+		try {
+			// 1.Recebe/faz a conexão do banco de dados
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "student", "22071991");
+			System.out.println("Conexão feita com sucesso!\n");
+		
+			// 2. Criação do 
+			myStmt = myConn.createStatement();
+			
+			//3. Execução das query SQL
+			myResul = myStmt.executeQuery("select * from employees");
+			
+			//4. Processamento do result set
+			while(myResul.next()) {
+				System.out.println(myResul.getString("last_name") + " " + myResul.getString("first_name") + " | " + myResul.getString("email"));
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+	}
+}
